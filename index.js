926d3bbc15f758c0f04bd96af0d58599cdfe9302
miriam <miriamwangui525@gmail.com>
2023-12-20 22:45:05 +0300

    Document object model fist project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+  
+    document.body.appendChild(newHeader);
+  
+    const yourName = 'Miriam'; 
+    newHeader.innerHTML = `${yourName} is the champion`;
+  
+     newHeader;
+  
+ 
